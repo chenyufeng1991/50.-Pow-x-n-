75f5b194fd468029ebd5c95e51ccdff8725ea6a7
陈宇峰 <yufengcode@gmail.com>
2016-10-06 10:40:38 +0800

    init

--- 50. Pow(x, n)/main.cpp
+++ 50. Pow(x, n)/main.cpp
@@ -7,9 +7,74 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+double myPow(double x, int n)
+{
+    bool isFlow = false;
+    if(x == 1.0)
+    {
+        return 1.0;
+    }
+
+    if (x == -1.0)
+    {
+        if (n % 2 == 0)
+        {
+            return 1.0;
+        }
+        else
+        {
+            return -1.0;
+        }
+    }
+
+    if (n == -2147483648)
+    {
+        n = -2147483647;
+        isFlow = true;
+    }
+
+    double result = 1.0;
+    int absN = n >= 0 ? n : -n;
+    for (int i = 0; i < absN; i++)
+    {
+        result *= x;
+        if (n >= 0)
+        {
+            if(fabs(result) < 0.00001 && fabs(result) > 0)
+            {
+                return 0.00000;
+            }
+        }
+        else
+        {
+            if(fabs(1/result) < 0.00001 && fabs(1/result) > 0)
+            {
+                return 0.00000;
+            }
+        }
+    }
+
+    if (isFlow)
+    {
+        result *= x;
+    }
+
+    return n >= 0 ? result : 1/result;
+}
+
+/**
+ *  -1.00000
+ 2147483647
+ */
+int main(int argc, const char * argv[])
+{
+    double res = myPow(2.00000, -2147483648);
+    cout << res;
+
     return 0;
 }
